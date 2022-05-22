f6462b74602e5abe53a3154b961ac5d1fb1ba1be
Artem <kvar65@yandex.ru>
2022-05-22 15:07:33 +0300

    deploy7

--- backend/api/views.py
+++ backend/api/views.py
@@ -3,9 +3,9 @@ from http import HTTPStatus
 from django.shortcuts import get_list_or_404, get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 from djoser.views import UserViewSet
+from recipes.models import Cart, Favorite, Ingredient, Recipe, Subscribe, Tag
 from rest_framework import permissions, viewsets
 from rest_framework.response import Response
-from recipes.models import (Cart, Favorite, Ingredient, Recipe, Subscribe, Tag)
 from users.models import User
 
 from .filters import IngredientSearchFilter, RecipeFilters
