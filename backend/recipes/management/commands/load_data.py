d0f08309c8ff19ba3df98ec667cb1ea8c60372fd
Artem <kvar65@yandex.ru>
2022-05-26 21:21:38 +0300

    json try2

--- backend/recipes/management/commands/load_data.py
+++ backend/recipes/management/commands/load_data.py
@@ -7,11 +7,26 @@ from recipes.models import Ingredient
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        with open('data/ingredients.json', 'rb') as f:
-            data = json.load(f)
-            for i in data:
-                ingredient = Ingredient()
-                ingredient.name = i['name']
-                ingredient.measurement_unit = i['measurement_unit']
-                ingredient.save()
-                print(i['name'], i['measurement_unit'])
+        ingredients_file = open(
+            'recipes/management/commands/data/ingredients.json',
+            encoding='utf-8'
+        )
+        ingredients_str = ingredients_file.read()
+        ingredients_data = json.loads(ingredients_str)
+        for ingredient in ingredients_data:
+            name = ingredient['name']
+            measurement_unit = ingredient['measurement_unit']
+            Ingredient.objects.create(
+                name=name,
+                measurement_unit=measurement_unit,
+            )
+
+    # def handle(self, *args, **options):
+    #     with open('data/ingredients.json', 'rb') as f:
+    #         data = json.load(f)
+    #         for i in data:
+    #             ingredient = Ingredient()
+    #             ingredient.name = i['name']
+    #             ingredient.measurement_unit = i['measurement_unit']
+    #             ingredient.save()
+    #             print(i['name'], i['measurement_unit'])
